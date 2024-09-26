ef3c7164fe383ecb179f5108101d815fd687d4d6
Matty <54020396+matty-ross@users.noreply.github.com>
2024-09-26 19:18:54 +0200

    Created M:M table for songs and playlists.

--- db/create.js
+++ db/create.js
@@ -1,6 +1,8 @@
 import sqlite from 'node:sqlite';
 
+
 const database = new sqlite.DatabaseSync('./db.sqlite3');
+
 
 database.exec(`
     CREATE TABLE "song" (
@@ -15,5 +17,12 @@ database.exec(`
     CREATE TABLE "playlist" (
         "id" INTEGER PRIMARY KEY,
         "name" VARCHAR(255) NOT NULL
+    );
+`);
+
+database.exec(`
+    CREATE TABLE "song_playlist" (
+        "song_id" INTEGER NOT NULL REFERENCES "song" ("id"),
+        "playlist_id" INTEGER NOT NULL REFERENCES "playlist" ("id")
     );
 `);
